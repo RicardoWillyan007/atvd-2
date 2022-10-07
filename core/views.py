85d5222cde1968bbdb8edac63578de8ea907cfe3
Ricardo <ricardowillyan2016@gmail.com>
2022-10-07 12:33:58 -0300

    first commit

---
+++ core/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+def perfil(request):
+    return render(request, 'perfil.html')
+
+
+
+# Create your views here.
